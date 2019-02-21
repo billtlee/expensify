6f8062c8a36e5516f69b40dad236827ecb3a8e89
billtlee <wlee3@hotmail.com>
2019-02-22 06:58:30 +0800

    upgrade babel

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -2,7 +2,7 @@ import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
 
 test('should setup remove expense action object', () => {
   const action = removeExpense({ id: '123abc' });
-  expect(action).toBe({
+  expect(action).toEqual({
     type: 'REMOVE_EXPENSE',
     id: '123abc'
   });
